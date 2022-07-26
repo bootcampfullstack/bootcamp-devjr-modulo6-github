914c9715fc8b1c40fdc31c13c1f267e1f5cf227f
Glauco Todesco <glauco.todesco@abutua.com>
2022-07-26 10:02:05 -0300

    refactored code

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-function search(){
+function search() {
     var username = document.getElementById("inputUserName").value;
 
     var url = `https://api.github.com/users/${username}`;
 
     $.getJSON(url, (user) => {
-        setUserData(user);
+        showUserData(user);
         clearError();
-    }).fail( () => {
-        setUserData({});
+    }).fail(() => {
+        showUserData({});
         showError("Não encontrado!");
     });
 
 }
 
-function showError(msg){
-    document.getElementById("error").innerHTML = `<div class='alert alert-danger mt-1' role='aler'>${msg}</div>`;
+function showError(msg) {
+    document.getElementById("error").innerHTML = `<div class='alert alert-danger mt-1' role='alert'>${msg}</div>`;
 }
 
-function clearError(){
+function clearError() {
     document.getElementById("error").innerHTML = "";
 }
 
 
-function setUserData(user){
+function showUserData(user) {
+    document.getElementById("name").innerHTML = user.name || "";
+    document.getElementById("html_url").innerHTML = user.html_url || "";
+    document.getElementById("company").innerHTML = user.company || "";
 
-    document.getElementById("name").innerHTML       = user.name     || "";
-    document.getElementById("html_url").innerHTML   = user.html_url || "";
-    document.getElementById("company").innerHTML    = user.company  || ""; 
-
-    document.getElementById("avatar_url").innerHTML = user.avatar_url ?   
-                                                      `
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ?
+        `
                                                        <img  src=${user.avatar_url} width="220" height="220" class="shadow rounded">
                                                       ` :
-                                                      "";
-                                                      
+        "";
 }
