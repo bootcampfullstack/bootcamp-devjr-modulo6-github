e6d4a07cad82f6b47122a99bce2ac2db3b9635cc
Glauco Todesco <glauco.todesco@abutua.com>
2022-07-26 08:44:38 -0300

    closes #3 - Handle Error

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-
 function search(){
     var username = document.getElementById("inputUserName").value;
 
     var url = `https://api.github.com/users/${username}`;
 
     $.getJSON(url, (user) => {
-
-        document.getElementById("name").innerHTML       = user.name;
-        document.getElementById("html_url").innerHTML   = user.html_url;
-        document.getElementById("company").innerHTML    = user.company;
-
-        document.getElementById("avatar_url").innerHTML    = `
-            <img  src=${user.avatar_url} width="220" height="220" class="shadow rounded">
-        `
-
-        
-
+        setUserData(user);
+        clearError();
+    }).fail( () => {
+        setUserData({});
+        showError("Não encontrado!");
     });
 
+}
+
+function showError(msg){
+    document.getElementById("error").innerHTML = `<div class='alert alert-danger mt-1' role='aler'>${msg}</div>`;
+}
+
+function clearError(){
+    document.getElementById("error").innerHTML = "";
+}
+
+
+function setUserData(user){
+
+    document.getElementById("name").innerHTML       = user.name     || "";
+    document.getElementById("html_url").innerHTML   = user.html_url || "";
+    document.getElementById("company").innerHTML    = user.company  || ""; 
+
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ?   
+                                                      `
+                                                       <img  src=${user.avatar_url} width="220" height="220" class="shadow rounded">
+                                                      ` :
+                                                      "";
+                                                      
 }
